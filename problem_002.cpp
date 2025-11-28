0bc3e3b743b626296cc7a3df0ddfff6bcf5b5e6f
Yogesh Kumar <tamparayogesh@gmail.com>
2025-11-28 20:14:01 +0530

    Solve problem #2: maximum subarray sum

--- problem_002.cpp
+++ problem_002.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: implement solution for problem $i
-    return 0;
+    int n;
+    if (!(cin >> n)) return 0;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    long long best = a[0], cur = a[0];
+    for (int i = 1; i < n; i++) {
+        cur = max(a[i], cur + a[i]);
+        best = max(best, cur);
+    }
+    cout << best;
 }
