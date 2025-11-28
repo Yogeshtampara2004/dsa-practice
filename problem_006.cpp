ce42a579d968fe4bf8e47e6c91b7834311d678b4
Yogesh Kumar <tamparayogesh@gmail.com>
2025-11-28 20:16:34 +0530

    Solve problem #6: valid anagram

--- problem_006.cpp
+++ problem_006.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: implement solution for problem $i
-    return 0;
+    string s, t;
+    if (!(cin >> s >> t)) return 0;
+    if (s.size() != t.size()) {
+        cout << "NO";
+        return 0;
+    }
+    vector<int> cnt(256);
+    for (char c : s) cnt[(unsigned char)c]++;
+    for (char c : t) cnt[(unsigned char)c]--;
+    for (int x : cnt) {
+        if (x != 0) {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
 }
