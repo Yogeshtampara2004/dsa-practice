2ae279bc93ae59752ea3a231eb5045720da6155d
Yogesh Kumar <tamparayogesh@gmail.com>
2025-11-28 20:17:10 +0530

    Solve problem #8: subarray sum equals k

--- problem_008.cpp
+++ problem_008.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: implement solution for problem $i
-    return 0;
+    int n;
+    long long k;
+    if (!(cin >> n >> k)) return 0;
+    vector<long long> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    unordered_map<long long,long long> mp;
+    mp[0] = 1;
+    long long sum = 0, ans = 0;
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        auto it = mp.find(sum - k);
+        if (it != mp.end()) ans += it->second;
+        mp[sum]++;
+    }
+    cout << ans;
 }
