754483cba3a21960cdbddb30094294934b473948
Yogesh Kumar <tamparayogesh@gmail.com>
2025-11-28 20:19:03 +0530

    Solve problem #9: binary search

--- problem_009.cpp
+++ problem_009.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: implement solution for problem $i
-    return 0;
+    int n, x;
+    if (!(cin >> n >> x)) return 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int l = 0, r = n - 1, ans = -1;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (a[m] == x) {
+            ans = m;
+            break;
+        } else if (a[m] < x) l = m + 1;
+        else r = m - 1;
+    }
+    cout << ans;
 }
