4c43b6334c07dbbe2431e26e72ea18953d1a7687
Yogesh Kumar <tamparayogesh@gmail.com>
2025-11-28 20:19:50 +0530

    Solve problem #10: merge sort

--- problem_010.cpp
+++ problem_010.cpp
@@ -1,9 +1,31 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+void ms(vector<int> &a, int l, int r) {
+    if (l >= r) return;
+    int m = l + (r - l) / 2;
+    ms(a, l, m);
+    ms(a, m + 1, r);
+    int i = l, j = m + 1;
+    vector<int> t;
+    t.reserve(r - l + 1);
+    while (i <= m && j <= r) {
+        if (a[i] <= a[j]) t.push_back(a[i++]);
+        else t.push_back(a[j++]);
+    }
+    while (i <= m) t.push_back(a[i++]);
+    while (j <= r) t.push_back(a[j++]);
+    for (int k = 0; k < (int)t.size(); k++) a[l + k] = t[k];
+}
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: implement solution for problem $i
-    return 0;
+    int n;
+    if (!(cin >> n)) return 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    ms(a, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        if (i) cout << " ";
+        cout << a[i];
+    }
 }
