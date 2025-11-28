e4a2330c964ad6d30e81a95fb520155c54322219
Yogesh Kumar <tamparayogesh@gmail.com>
2025-11-28 20:11:28 +0530

    Solve problem #1: two sum

--- problem_001.cpp
+++ problem_001.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // TODO: implement solution for problem $i
-    return 0;
+    int n, target;
+    if (!(cin >> n >> target)) return 0;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    unordered_map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        int need = target - a[i];
+        auto it = mp.find(need);
+        if (it != mp.end()) {
+            cout << it->second << " " << i;
+            return 0;
+        }
+        mp[a[i]] = i;
+    }
+    cout << -1;
 }
