26f8650b15ef8ccfa0d068cea383d6d472460414
Yogesh Kumar <tamparayogesh@gmail.com>
2025-11-28 20:05:09 +0530

    Solve problem #10: add solution file problem_010.cpp

---
+++ problem_010.cpp
@@ -0,0 +1,9 @@
+﻿#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // TODO: implement solution for problem $i
+    return 0;
+}
